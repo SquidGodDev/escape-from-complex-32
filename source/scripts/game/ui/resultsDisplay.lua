562a41834149bc7a10e3813ed6750eda923f32ea
SquidGodDev <tako48188@gmail.com>
2022-05-25 23:45:34 -0700

    Adjusted results display animation timings

--- source/scripts/game/ui/resultsDisplay.lua
+++ source/scripts/game/ui/resultsDisplay.lua
@@ -15,12 +15,12 @@ function ResultsDisplay:init(gameScene, height, snapshot)
     self.animatingIn = true
     self.animatingOut = false
     self.animateInDuration = 1000
-    self.animateOutDuration = 1000
+    self.animateOutDuration = 500
     self.snapshotAnimator = gfx.animator.new(self.animateInDuration, 1, 0.5, pd.easingFunctions.linear)
-    self.lastDrawnBlurred = -1
+    self.lastDrawnFaded = 1
 
     self.heightDialog = HeightDialog(height)
-    self.dialogAnimator = gfx.animator.new(self.animateInDuration, self.heightDialog.y, 120, pd.easingFunctions.inOutCubic)
+    self.dialogAnimator = gfx.animator.new(self.animateInDuration / 2, self.heightDialog.y, 120, pd.easingFunctions.inOutCubic)
 
     self:setImage(self.snapshot)
     self:setZIndex(500)
@@ -32,16 +32,7 @@ end
 function ResultsDisplay:update()
     if self.animatingIn then
         local snapshotAnimatorValue = self.snapshotAnimator:currentValue()
-        -- local blurAmount = math.floor(snapshotAnimatorValue)
-        -- if blurAmount ~= self.lastDrawnBlurred then
-        --     print(blurAmount)
-        --     self.lastDrawnBlurred = blurAmount
-        --     local blurredImage = gfx.image.new(400, 240)
-        --     gfx.pushContext(blurredImage)
-        --         -- self.snapshot:drawBlurred(0, 0, blurAmount, 1, gfx.image.kDitherTypeFloydSteinberg)
-        --     gfx.popContext()
-        --     self:setImage(self.snapshot:vcrPauseFilterImage())
-        -- end
+        self.lastDrawnFaded = snapshotAnimatorValue
         local fadedImage = gfx.image.new(400, 240)
         gfx.pushContext(fadedImage)
             self.snapshot:drawFaded(0, 0, snapshotAnimatorValue, gfx.image.kDitherTypeBayer8x8)
